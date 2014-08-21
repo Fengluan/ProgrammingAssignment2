1c2f907c2c4418f914443c5b79f0785a1803be35
Fengluan <693264936@qq.com>
2014-08-21 09:22:02 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,20 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	y <- as.vector(x)
+	n <- length(y)
+	m <- floor(sqrt(n))
+	y1 <- y[1:m^2]
+	x1 <- matrix(y1,m,m)
+	return(x1)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    y <- makeCacheMatrix(x)
+    y1 <- solve(y)
+	return(y1)
+## Return a matrix that is the inverse of 'x'
 }
